ac9237a3974dfda9dd1d273ff5561f6f7ae6ce4c
Lucas <lucas.araujo@icomp.ufam.edu.br>
2025-12-11 19:34:27 -0400

    navbar created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './core/components/navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: Navbar, title: "Home"}
+];
